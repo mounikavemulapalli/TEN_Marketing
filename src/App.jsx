b46b8561eda9854b3a2225434bb68d87024be12d
Himanshu Singh Rajput <158039836+Himanshu0721@users.noreply.github.com>
2024-09-30 19:57:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Services from './Components/Services';
 import JoinUs from './Components/JoinUs';
 import Partners from './Components/Partners';
 import Client from "./Components/Client";
-import Home from './Components/Home'
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+
 
 export default function App() {
   return (
