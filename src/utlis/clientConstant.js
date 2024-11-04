8982e9f47a7db23a20ab5a70dcd49d732d73b5cc
samikshasahare31 <samikshasahare31@gmail.com>
2024-11-04 16:17:17 +0530

    added files for client

---
+++ src/utlis/clientConstant.js
@@ -0,0 +1,58 @@
+export const companyArray =[
+    {
+      img:"https://static.wixstatic.com/media/63dca8_513f58f8b01f42a1ab14b713077240b1~mv2.jpg/v1/fill/w_113,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_513f58f8b01f42a1ab14b713077240b1~mv2.jpg",
+      companyName:"COMPANY NAME",
+      description:"Something about this company and its working",
+   },
+   {
+      img:"https://static.wixstatic.com/media/63dca8_513f58f8b01f42a1ab14b713077240b1~mv2.jpg/v1/fill/w_113,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_513f58f8b01f42a1ab14b713077240b1~mv2.jpg",
+      companyName:"COMPANY NAME",
+      description:"Something about this company and its working",
+   },
+   {
+      img:"https://static.wixstatic.com/media/63dca8_f2cf8f2ea2834ddd876151415c88159f~mv2.jpg/v1/fill/w_113,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_f2cf8f2ea2834ddd876151415c88159f~mv2.jpg",
+      companyName:"COMPANY NAME",
+      description:"Something about this company and its working",
+   },
+   {
+      img:"https://static.wixstatic.com/media/63dca8_7abd193e13754bf1be830d13d4f0885a~mv2.jpg/v1/fill/w_113,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_7abd193e13754bf1be830d13d4f0885a~mv2.jpg",
+      companyName:"COMPANY NAME",
+      description:"Something about this company and its working",
+   },
+   {
+      img:"https://static.wixstatic.com/media/63dca8_b207f93ea2254fe9b7ee63d7208e3105~mv2.jpg/v1/fill/w_110,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_b207f93ea2254fe9b7ee63d7208e3105~mv2.jpg",
+      companyName:"COMPANY NAME",
+      description:"Something about this company and its working",
+   },
+   {
+      img:"https://static.wixstatic.com/media/63dca8_5c0b9cfb001949b7887c12680076c4c0~mv2.jpg/v1/fill/w_110,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_5c0b9cfb001949b7887c12680076c4c0~mv2.jpg",
+      companyName:"COMPANY NAME",
+      description:"Something about this company and its working",
+   },
+   {
+     img:"https://static.wixstatic.com/media/63dca8_b461c0dfb3f145e18f7b20054befe72f~mv2.jpg/v1/fill/w_110,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_b461c0dfb3f145e18f7b20054befe72f~mv2.jpg",
+     companyName:"COMPANY NAME",
+     description:"Something about this company and its working",
+  },
+  {
+     img:"https://static.wixstatic.com/media/63dca8_98ecba5bb09b40d78c858cac6c4f40a4~mv2.jpg/v1/fill/w_110,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_98ecba5bb09b40d78c858cac6c4f40a4~mv2.jpg",
+     companyName:"COMPANY NAME",
+     description:"Something about this company and its working",
+  },
+  {
+     img:"https://static.wixstatic.com/media/63dca8_a04c41a2dc41454c835a74fdc56729bc~mv2.jpg/v1/fill/w_110,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_a04c41a2dc41454c835a74fdc56729bc~mv2.jpg",
+     companyName:"COMPANY NAME",
+     description:"Something about this company and its working",
+  },
+  {
+     img:"https://static.wixstatic.com/media/63dca8_653192bf3d7d459e94b1c91dcb86de86~mv2.jpg/v1/fill/w_110,h_110,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/63dca8_653192bf3d7d459e94b1c91dcb86de86~mv2.jpg",
+     companyName:"COMPANY NAME",
+     description:"Something about this company and its working",
+  },
+  {
+     img:"https://static.wixstatic.com/media/1ecb1b_a816e246a3aa42e89083ec9a9e855497~mv2.png/v1/fill/w_129,h_129,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1ecb1b_a816e246a3aa42e89083ec9a9e855497~mv2.png",
+     companyName:"COMPANY NAME",
+     description:"Something about this company and its working",
+  },
+  
+  ]
