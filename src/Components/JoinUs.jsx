901230d213f4c73628ccb22f642f9b22f49dabc3
Himanshu Singh Rajput <158039836+Himanshu0721@users.noreply.github.com>
2024-11-11 20:46:51 +0530

    Update JoinUs.jsx

--- src/Components/JoinUs.jsx
+++ src/Components/JoinUs.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+// JoinUs.jsx
+import React from 'react';
+import './joinUs.css';
 
-const JoinUs = () => {
+function JoinUs() { 
   return (
-    <div>
-      JoinUs
+    <div className="JoinUs">
+      <section className="feedback">
+        <h2>Give Us Your Feedback</h2>
+        <form>
+          <div className="input-group">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div className="input-group">
+            <input type="email" placeholder="Email" />
+            <input type="tel" placeholder="Phone" />
+          </div>
+          <textarea placeholder="Write your feedback here"></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+
+   
     </div>
-  )
+  );
 }
 
-export default JoinUs
+export default JoinUs;
